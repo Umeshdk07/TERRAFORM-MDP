793ccce0c27130be8e7ec15f6b15f18d787087b2
Umeshdk07 <umeshrdk1997@gmail.com>
2025-08-25 20:01:07 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,146 @@
+provider "azurerm" {
+  features {}
+  subscription_id = var.sub_id
+}
+
+resource "azurerm_resource_group" "private" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  address_space       = [var.vnet_address_space]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.private.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.private.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [var.subnet_prefix]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = var.nic_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.private.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = var.nsg_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.private.name
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                            = var.vm_name
+  resource_group_name             = azurerm_resource_group.private.name
+  location                        = var.location
+  size                            = var.vm_size
+  admin_username                  = var.admin_username
+  admin_password                  = var.admin_password
+  disable_password_authentication = false
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  source_image_reference {
+    publisher = var.image_publisher
+    offer     = var.image_offer
+    sku       = var.image_sku
+    version   = var.image_version
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = var.os_disk_type
+    disk_size_gb         = 256
+  }
+}
+
+resource "azurerm_managed_disk" "extra_disks" {
+  count                = var.disks_count
+  name                 = "${var.vm_name}-datadisk-${count.index}"
+  location             = var.location
+  resource_group_name  = azurerm_resource_group.private.name
+  storage_account_type = "Standard_LRS"
+  disk_size_gb         = var.disks_size
+  create_option        = "Empty"
+}
+
+resource "azurerm_virtual_machine_data_disk_attachment" "extra_disk_attach" {
+  count              = var.disks_count
+  managed_disk_id    = azurerm_managed_disk.extra_disks[count.index].id
+  virtual_machine_id = azurerm_linux_virtual_machine.vm.id
+  lun                = count.index
+  caching            = "ReadWrite"
+}
+
+resource "azurerm_dev_test_global_vm_shutdown_schedule" "shutdown" {
+  virtual_machine_id    = azurerm_linux_virtual_machine.vm.id
+  location              = var.location
+  enabled               = true
+  daily_recurrence_time = "1900"
+  timezone              = "India Standard Time"
+
+  notification_settings {
+    enabled         = false
+    time_in_minutes = 30
+    webhook_url     = ""
+  }
+}
+
+data "azurerm_virtual_network" "jenkins" {
+  name                = var.jenkins_peering_vnet_name
+  resource_group_name = "amdp-jenkins"
+}
+
+data "azurerm_virtual_network" "development" {
+  name                = var.development_peering_vnet_name
+  resource_group_name = "amdp-development2"
+}
+
+resource "azurerm_virtual_network_peering" "private_to_jenkins" {
+  name                      = "private-to-jenkins-mdp"
+  resource_group_name       = azurerm_resource_group.private.name
+  virtual_network_name      = azurerm_virtual_network.vnet.name
+  remote_virtual_network_id = data.azurerm_virtual_network.jenkins.id
+  allow_forwarded_traffic   = true
+  allow_gateway_transit     = false
+  use_remote_gateways       = false
+}
+
+resource "azurerm_virtual_network_peering" "private_to_development" {
+  name                      = "private-to-development-mdp"
+  resource_group_name       = azurerm_resource_group.private.name
+  virtual_network_name      = azurerm_virtual_network.vnet.name
+  remote_virtual_network_id = data.azurerm_virtual_network.development.id
+  allow_forwarded_traffic   = true
+  allow_gateway_transit     = false
+  use_remote_gateways       = false
+}
